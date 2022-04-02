66da8dbad3b062e3f142a042d9dfd0d811dc336a
Lucas <bazezimo42@gmail.com>
2022-04-02 02:59:23 -0300

    O BB largou a chupeta

--- scripts.js
+++ scripts.js
@@ -9,5 +9,9 @@ function precoSite() {
     document.querySelector(".preco-item p").innerHTML = precificacao(valor);
     item.classList.add("preco-real");
     item.classList.remove("preco-item");
+
+    if(item) {
+        precoSite()
+    }
 }
 precoSite()
