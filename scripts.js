789262144db434131cb57535986977117d7e0b67
Lucas <bazezimo42@gmail.com>
2022-04-02 01:56:11 -0300

    Tá falando papai

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// Valores
+
+function precoSite() {
+    let item = document.querySelector(".preco-item");
+    let valor = document.querySelector(".preco-item p").innerHTML;
+    const precificacao = function(preco){
+        return new Intl.NumberFormat('pt-br', {style: 'currency',currency: 'BRL', minimumFractionDigits: 2}).format(preco);
+        };
+    document.querySelector(".preco-item p").innerHTML = precificacao(valor);
+    item.classList.add("preco-real");
+    item.classList.remove("preco-item");
+}
+precoSite()
